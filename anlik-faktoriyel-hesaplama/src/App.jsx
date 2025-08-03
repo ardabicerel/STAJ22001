367107b2ec4d761ed2d190ed918d64bf264c188d
ardabicerel <ardabicerel@hotmail.com>
2025-08-04 00:15:51 +0300

    Faktoriyel işlemi çalışıyor. Proje tamamlandı. useState ve useEffect kullanıldı.

--- anlik-faktoriyel-hesaplama/src/App.jsx
+++ anlik-faktoriyel-hesaplama/src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [number, setNumber] = useState("")
+  const [faktoriyell, setFaktoriyell] = useState("")
+
+  const faktoriyelHesapla = (sayi) => {
+    let sonuc = 1;
+    for(let i = 1; i <= sayi; i++) {
+      sonuc*= i;
+    }
+    return setFaktoriyell(sonuc);
+  }
+
+  useEffect(() => {
+    if (number !== "") {
+      faktoriyelHesapla(number);}
+    else{
+      setFaktoriyell("");
+    }
+    }, [number]);
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='grid lg:grid-cols-2 grid-cols-1 gap-10 p-4'>
+
+        <div className='lg:col-span-1 col-span-1'>
+          <label className='block mb-2 text-left'>Girilen Sayı :</label>
+          <input name="sayi" type="number" className='bg-[#999] text-black' onChange={(event) => setNumber(event.target.value)}></input>
+        </div>
+
+        
+        <div className='lg:col-span-1 col-span-1'>
+          <label className='block mb-2 text-left'>Faktoriyel Sonucu :</label>
+          <input value={faktoriyell} className='bg-[#999] text-black' readonly></input>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
