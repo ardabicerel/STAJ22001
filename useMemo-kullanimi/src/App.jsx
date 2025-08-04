daf62a904fe0be158668aec42b741aba2ea39b55
ardabicerel <ardabicerel@hotmail.com>
2025-08-04 17:42:31 +0300

    useMemo hookunun yararı ile ilgili örnek yapıldı.

--- useMemo-kullanimi/src/App.jsx
+++ useMemo-kullanimi/src/App.jsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useMemo } from 'react'
 import './App.css'
 
+const maliyetliHesaplama = (sayi) => {
+  console.time('Hesaplama Süresi')
+  let toplam = 0
+  for(let i = 0; i <= sayi; i++){
+    toplam += i
+  }
+  console.timeEnd('Hesaplama Süresi')
+  return toplam
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [sayi, setSayi] = useState(0)
+  const [counter, setCounter] = useState(0)
+
+  const toplaminSonucu = useMemo(() => {
+    return maliyetliHesaplama(100000)
+
+  }, [sayi])
+  
+
 
   return (
     <>
-      
+      <div>
+        <h3>Hesaplama Sonucu : {toplaminSonucu}</h3>
+        <button onClick={() => setCounter(counter + 1)}>Sayacı Arttır : {counter}</button>
+        <p>Bu butona her tıkladığınızda, yukarıdaki maliyetli hesaplama artık yeniden çalışmayacak.</p>
+      </div>
     </>
   )
 }
