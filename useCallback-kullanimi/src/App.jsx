a46cabb0cd7263e3071b2f62a27785fa34531ea0
ardabicerel <ardabicerel@hotmail.com>
2025-08-05 19:36:28 +0300

    useCallback kullanılarak avantajları incelendi.

--- useCallback-kullanimi/src/App.jsx
+++ useCallback-kullanimi/src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
+import React, {useState, useCallback} from 'react'
 import './App.css'
 
-function App() {
- 
+
+const ChildComponent = React.memo(({onClick}) => {
+    console.log("ChildComponent yeniden render edildi.");
+    return <button onClick={onClick}>Sayacı Arttır</button>
+})
 
 
+function App() {  
+
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = useCallback(() => {
+    setCount(count + 1);
+  }, [])
 
   return (
     <>
-      
+      <div>
+        <h1>Sayaç: {count}</h1>
+        <ChildComponent onClick={handleIncrement} />
+      </div>
     </>
   )
 }
